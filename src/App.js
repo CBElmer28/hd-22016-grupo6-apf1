986233812699591944cc4352d307642e45b70f69
Jhon3125 <sotojonathan070@gmail.com>
2025-04-28 22:48:46 -0500

    cargando los react-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Modelos from './components/Servicios/Modelos.jsx'; // Asegúrate de que esté la ruta correcta
+import Servicios from './components/Servicios/PrincipalServicio.jsx'; // Asegúrate de que esté la ruta correcta
 import './css/Modelos.css';
 
 function App() {
   return (
     <div>
-      <Modelos />
+      <Servicios />
     </div>
   );
 }
