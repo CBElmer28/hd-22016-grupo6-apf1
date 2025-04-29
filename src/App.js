c8d56a6261354ae5b39cb8ab6c1da50f84075e5c
Jhon3125 <sotojonathan070@gmail.com>
2025-04-28 23:36:30 -0500

    Ahora si crj

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Servicios/PrincipalServicio';
 import Modelos from './components/Servicios/Modelos';
 import Servicios from './components/Servicios/PrincipalServicio';
+import CompraServicios from './components/Servicios/CompraServicios';
 // import Marca from './components/Servicios/Marca'; // Necesitarás crear este componente
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/modelos" element={<Modelos />} />
         <Route path="/services" element={<Servicios />} />
+        <Route path="/compraservicios" element={<CompraServicios />} />
         {/* <Route path="/marca" element={<Marca />} />  */}
       </Routes>
     </Router>
