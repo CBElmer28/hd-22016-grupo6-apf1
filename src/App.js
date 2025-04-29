52cafd4ef9b1a63c1d018c597564416e8a66906e
Jhon3125 <sotojonathan070@gmail.com>
2025-04-28 23:24:32 -0500

    Navbar casi completo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/General/Navbar';
-import Footer from './components/General/Footer';
 import Home from './components/Servicios/PrincipalServicio';
 import Modelos from './components/Servicios/Modelos';
 import Servicios from './components/Servicios/PrincipalServicio';
@@ -9,19 +7,12 @@ import Servicios from './components/Servicios/PrincipalServicio';
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/modelos" element={<Modelos />} />
-        <Route path="/company" element={<Company />} />
         <Route path="/services" element={<Servicios />} />
-        <Route path="/marca" element={<Marca />} />
-        <Route path="/proveedor" element={<Proveedor />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/buscar" element={<Buscar />} />
-        <Route path="/favoritos" element={<Favoritos />} />
+        {/* <Route path="/marca" element={<Marca />} />  */}
       </Routes>
-      <Footer />
     </Router>
   );
 }
