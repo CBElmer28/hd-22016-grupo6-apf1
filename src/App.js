d95eef4938a476e6db40466d8061e41df8492daa
Jhon3125 <sotojonathan070@gmail.com>
2025-04-28 23:13:25 -0500

    ajá

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import Servicios from './components/Servicios/PrincipalServicio.jsx'; // Asegúrate de que esté la ruta correcta
-import './css/Modelos.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/General/Navbar';
+import Footer from './components/General/Footer';
+import Home from './components/Servicios/PrincipalServicio';
+import Modelos from './components/Servicios/Modelos';
+import Servicios from './components/Servicios/PrincipalServicio';
+// import Marca from './components/Servicios/Marca'; // Necesitarás crear este componente
 
 function App() {
   return (
-    <div>
-      <Servicios />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/modelos" element={<Modelos />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/services" element={<Servicios />} />
+        <Route path="/marca" element={<Marca />} />
+        <Route path="/proveedor" element={<Proveedor />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/buscar" element={<Buscar />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
